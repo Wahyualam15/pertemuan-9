4c62c987e41ae03ea10e0f2709d7d2a95a4d8e4f
Wahyu Nur Alam <79901656+Wahyualam15@users.noreply.github.com>
2021-03-17 08:17:22 +0700

    Add files via upload

---
+++ pert9_latihan4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanggal</title>
+	<style type="text/css">
+		h3	{
+			color: red;
+			background-color: gray;
+		}
+	</style>
+</head>
+<body>
+	<font size="10px">
+		<h3>FUNGSI DATE</h3>
+		<h3>WahyuNurAlam</h3>
+	<?php 
+	echo "Sekarang Tanggal";
+	echo date('d-F-Y');
+	echo "<br> dan jam";
+	echo date('h:i:s A');
+	 ?>
+	</font>
+</body>
+</html>
